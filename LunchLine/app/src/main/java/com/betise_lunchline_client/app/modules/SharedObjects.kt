e35081bcd32fee968180cb7da79748f548a251b1
Aakarsh Jain <jainaakarsh09@gmail.com>
2023-01-08 05:21:51 +0530

    Added List for menu at Homepage

--- LunchLine/app/src/main/java/com/betise_lunchline_client/app/modules/SharedObjects.kt
+++ LunchLine/app/src/main/java/com/betise_lunchline_client/app/modules/SharedObjects.kt
@@ -1,6 +1,8 @@
 package com.betise_lunchline_client.app.modules
 
 import android.annotation.SuppressLint
+import androidx.annotation.Keep
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -13,9 +15,21 @@ class SharedObjects {
         val Profile: String,
         val OpenID: String,
     )
+    data class Dish(
+        val ItemName: String,
+        val ItemCost: Long,
+        val Desc: String,
+        val CurrentAvailability: Boolean,
+        val Rating: Double,
+        val StartTime: Timestamp,
+        val EndTime: Timestamp,
+        val Days: ArrayList<Boolean>,
+    )
     companion object{
         @SuppressLint("StaticFieldLeak")
         val db = Firebase.firestore
+        val menuCollection = db.collection("vendors").document("pqCOEjacUXj6r5KM7fEL").collection("menu")
+        val dishes:MutableList<Dish> = mutableListOf<Dish>()
     }
 
 }
