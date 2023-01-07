c459c4b398876de2be71da1e39934ba0f82fdccc
Atharv <atharvgoel03@gmail.com>
2023-01-08 01:32:03 +0300

    ordered received components done

--- LunchLine/app/src/main/java/com/betise_lunchline_client/app/modules/orderrecieved/ui/OrderRecievedActivity.kt
+++ LunchLine/app/src/main/java/com/betise_lunchline_client/app/modules/orderrecieved/ui/OrderRecievedActivity.kt
@@ -3,6 +3,11 @@ package com.betise_lunchline_client.app.modules.orderrecieved.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.viewModels
 import com.betise_lunchline_client.app.R
 import com.betise_lunchline_client.app.appcomponents.base.BaseActivity
@@ -16,25 +21,43 @@ import kotlin.Unit
 class OrderRecievedActivity :
     BaseActivity<ActivityOrderRecievedBinding>(R.layout.activity_order_recieved) {
   private val viewModel: OrderRecievedVM by viewModels<OrderRecievedVM>()
+  private lateinit var orderedItemsContainer : LinearLayout
 
   override fun onInitialized(): Unit {
     viewModel.navArguments = intent.extras?.getBundle("bundle")
     binding.orderRecievedVM = viewModel
-  }
+
+    orderedItemsContainer = findViewById(R.id.order_done_items_container)
+    val inflater: android.view.LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as
+            android.view.LayoutInflater
+
+    val orderedLength = 4
+    for (i in 1..orderedLength) {
+      // Create the ordered item
+      val orderItemComponent: View = inflater.inflate(R.layout.order_received_component, null)
+      val reviewBox: EditText = orderItemComponent.findViewById<EditText>(R.id.reviewInput)
+      val itemImg: ImageView = orderItemComponent.findViewById<ImageView>(R.id.itemImageView)
+
+      orderItemComponent.findViewById<TextView>(R.id.itemName).text = "Item $i"
+      orderItemComponent.findViewById<TextView>(R.id.itemPrice).text = "Price $i"
+
+      orderedItemsContainer.addView(orderItemComponent)
+      }
+    }
 
   override fun setUpClicks(): Unit {
-    binding.linearRowpngeggninetynineOne.setOnClickListener {
-      val destIntent = DishPageActivity.getIntent(this, null)
-      startActivity(destIntent)
-    }
+//    binding.linearRowpngeggninetynineOne.setOnClickListener {
+//      val destIntent = DishPageActivity.getIntent(this, null)
+//      startActivity(destIntent)
+//    }
     binding.btnGoToHome.setOnClickListener {
       val destIntent = HomePageActivity.getIntent(this, null)
       startActivity(destIntent)
     }
-    binding.linearRowpngeggninetynine.setOnClickListener {
-      val destIntent = DishPageActivity.getIntent(this, null)
-      startActivity(destIntent)
-    }
+//    binding.linearRowpngeggninetynine.setOnClickListener {
+//      val destIntent = DishPageActivity.getIntent(this, null)
+//      startActivity(destIntent)
+//    }
   }
 
   companion object {
