0cffc6161be5194732fa46c30c42040706fefbb5
Atharv <atharvgoel03@gmail.com>
2023-01-07 23:50:29 +0300

    cart page components done
    some elements appear to be positioned less than ideally. Do not modify anything other than cart_item_component.xml

--- LunchLine/app/src/main/java/com/betise_lunchline_client/app/modules/cart/ui/CartActivity.kt
+++ LunchLine/app/src/main/java/com/betise_lunchline_client/app/modules/cart/ui/CartActivity.kt
@@ -3,6 +3,9 @@ package com.betise_lunchline_client.app.modules.cart.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.viewModels
 import com.betise_lunchline_client.app.R
 import com.betise_lunchline_client.app.appcomponents.base.BaseActivity
@@ -16,10 +19,33 @@ import kotlin.Unit
 
 class CartActivity : BaseActivity<ActivityCartBinding>(R.layout.activity_cart) {
   private val viewModel: CartVM by viewModels<CartVM>()
+  private lateinit var cartItemsContainer : LinearLayout
 
   override fun onInitialized(): Unit {
     viewModel.navArguments = intent.extras?.getBundle("bundle")
     binding.cartVM = viewModel
+
+    cartItemsContainer = findViewById(R.id.cartItemsContainer)
+    val inflater: android.view.LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as
+            android.view.LayoutInflater
+
+    val cartLength = 4
+    for (i in 1..cartLength) {
+      // Create the cart item
+      val cartItem : View = inflater.inflate(R.layout.cart_item_component, null)
+      val cartItemData : LinearLayout = cartItem.findViewById<LinearLayout>(R.id.itemDataContainer)
+
+      // Set the item name, price, quantity
+      cartItemData.findViewById<TextView>(R.id.itemName).text = "Item #$i" // Item name
+      cartItemData.findViewById<TextView>(R.id.itemPrice).text = "Price: Rs. $i" // Item price
+      cartItemData.findViewById<LinearLayout>(R.id.linearRowfour).findViewById<TextView>(R.id.itemQuantity).text = "$i" // Item quantity
+
+//      val removeButton: AppCompatButton = layout.findViewById(R.id.btnRemove)
+//      removeButton.setOnClickListener {
+//        cartItemsContainer.removeView(cartItem)
+//      }
+      cartItemsContainer.addView(cartItem)
+    }
   }
 
   override fun setUpClicks(): Unit {
