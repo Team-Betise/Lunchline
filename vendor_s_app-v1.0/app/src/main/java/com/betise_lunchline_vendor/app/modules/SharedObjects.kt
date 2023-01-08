3377a8cbf318b1a82dc9b8150dd268935f32676a
Vartika2401 <97868917+Vartika2401@users.noreply.github.com>
2023-01-08 12:57:20 +0530

    Data saving done

---
+++ vendor_s_app-v1.0/app/src/main/java/com/betise_lunchline_vendor/app/modules/SharedObjects.kt
@@ -0,0 +1,29 @@
+package com.betise_lunchline_vendor.app.modules
+
+import android.annotation.SuppressLint
+import com.google.firebase.Timestamp
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.local.ReferenceSet
+
+
+class SharedObjects {
+    data class Dish(
+        val ItemName: String,
+        val ItemCost: Long,
+        val Desc: String,
+        val CurrentAvailability: Boolean,
+        val Rating: Double,
+        val ReviewCount: Long,
+        val StartTime: Timestamp,
+        val EndTime: Timestamp,
+        val Days: ArrayList<Boolean>,
+        val ETA: Long,
+    )
+    companion object{
+        @SuppressLint("StaticFieldLeak")
+        val db = Firebase.firestore
+        val menuCollection = db.collection("vendors").document("pqCOEjacUXj6r5KM7fEL").collection("menu")
+        var dishes:MutableList<Dish> = mutableListOf<Dish>()
+    }
+}
