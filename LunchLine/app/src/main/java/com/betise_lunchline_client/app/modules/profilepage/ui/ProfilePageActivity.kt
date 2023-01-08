58044f20f35cab9368da596a1a6d3fb201fdecf9
Aakarsh Jain <jainaakarsh09@gmail.com>
2023-01-08 08:27:20 +0530

    Fetching past order for profile
    Fetching for individual items in the order takes time and due to asynchronous nature of kotlin, ends up leading to errors

--- LunchLine/app/src/main/java/com/betise_lunchline_client/app/modules/profilepage/ui/ProfilePageActivity.kt
+++ LunchLine/app/src/main/java/com/betise_lunchline_client/app/modules/profilepage/ui/ProfilePageActivity.kt
@@ -4,13 +4,17 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.betise_lunchline_client.app.R
 import com.betise_lunchline_client.app.appcomponents.base.BaseActivity
 import com.betise_lunchline_client.app.databinding.ActivityProfilePageBinding
+import com.betise_lunchline_client.app.modules.SharedObjects
 import com.betise_lunchline_client.app.modules.profilepage.`data`.model.ListpngeggninetynineRowModel
 import com.betise_lunchline_client.app.modules.profilepage.`data`.model.ListpngeggninetynineTwoRowModel
 import com.betise_lunchline_client.app.modules.profilepage.`data`.viewmodel.ProfilePageVM
+import com.google.firebase.firestore.DocumentReference
+import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.Int
 import kotlin.String
 import kotlin.Unit
@@ -18,8 +22,12 @@ import kotlin.Unit
 class ProfilePageActivity : BaseActivity<ActivityProfilePageBinding>(R.layout.activity_profile_page)
 {
     private val viewModel: ProfilePageVM by viewModels<ProfilePageVM>()
+    private var orders: MutableList<SharedObjects.Order> = mutableListOf<SharedObjects.Order>()
 
     override fun onInitialized(): Unit {
+
+        getOrders()
+
         viewModel.navArguments = intent.extras?.getBundle("bundle")
         val listpngeggninetynineAdapter =
             ListpngeggninetynineAdapter(viewModel.listpngeggninetynineList.value?:mutableListOf())
@@ -73,6 +81,65 @@ class ProfilePageActivity : BaseActivity<ActivityProfilePageBinding>(R.layout.ac
     ): Unit {
         when(view.id) {
         }
+    }
+
+    private fun getOrders(){
+        SharedObjects.db.collection("orders")
+            .whereEqualTo("User", SharedObjects.db.collection("users").document(SharedObjects.user_email))
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    Toast.makeText(this, "Found a doc", Toast.LENGTH_SHORT).show()
+                    var order_items: ArrayList<SharedObjects.Item> = ArrayList()
+                    SharedObjects.db.collection("orders").document(document.id).collection("Items")
+                        .get()
+                        .addOnSuccessListener { items ->
+                            for (item in items) {
+                                val dish_ref = item.data["Item"] as DocumentReference
+                                var dish: SharedObjects.Dish? = null
+                                dish_ref.get()
+                                    .addOnSuccessListener { result ->
+                                        Toast.makeText(this, "Found a dish", Toast.LENGTH_SHORT).show()
+//                                        dish = SharedObjects.Dish(
+//                                            result.data["ItemName"] as String,
+//                                            document.data["ItemCost"] as Long,
+//                                            document.data["Desc"] as String,
+//                                            document.data["CurrentAvailability"] as Boolean,
+//                                            document.data["Rating"] as Double,
+//                                            document.data["StartTime"] as com.google.firebase.Timestamp,
+//                                            document.data["EndTime"] as com.google.firebase.Timestamp,
+//                                            document.data["Days"] as ArrayList<Boolean>,
+////                                        )
+                                    }
+                                    .addOnFailureListener { exception ->
+                                        Toast.makeText(this, "Failed to find a dish", Toast.LENGTH_SHORT).show()
+                                    }
+//                                val order_item = SharedObjects.Item(
+//                                    dish!! as SharedObjects.Dish,
+//                                    item.data["EndTime"] as com.google.firebase.Timestamp,
+//                                    item.data["Status"] as String,
+//                                )
+//                                order_items.add(order_item)
+                            }
+                        }
+                        .addOnFailureListener() { exception ->
+                            println("Error getting documents: $exception")
+                        }
+                    val order = SharedObjects.Order(
+                        document.data["Status"] as String,
+                        document.data["Amount"] as Long,
+                        document.data["ItemsRemaining"] as Long,
+                        order_items as ArrayList<SharedObjects.Item>,
+                        document.data["TimePlaced"] as com.google.firebase.Timestamp,
+                        document.data["VerificationCode"] as String,
+                    )
+                    orders.add(order)
+                    Toast.makeText(this, document.data["VerificationCode"] as String, Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener { exception ->
+                println("Error getting documents: $exception")
+            }
     }
 
     companion object {
